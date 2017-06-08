3940ea980d702cb377a3ebc21b18f8b586887261
Hong Ong <hong_ong@knorex.com>
2017-06-08 18:34:50 +0800

    Basics rough set on patients data

--- roughset_demo/basics.R
+++ roughset_demo/basics.R
@@ -1,2 +1,24 @@
+library(RoughSets)
+
 patients <- read.csv("patients.csv")
-patients
+decision.table <- SF.asDecisionTable(dataset = patients, decision.attr = 4, indx.nominal = c(1:4))
+
+P <- c(2,3)
+
+####### Perform indiscernibility relation #######
+IND <- BC.IND.relation.RST(decision.table, feature.set = P)
+
+####### Perform lower and upper approximatino #####
+roughset <- BC.LU.approximation.RST(decision.table, IND)
+
+####### Determine the positive region ######
+region <- BC.positive.reg.RST(decision.table, roughset) 
+
+print("Indiscernibility Relation:")
+print(IND)
+
+print("Lower and Upper Approximations:")
+print(roughset)
+
+print("Regions:")
+print(region)
