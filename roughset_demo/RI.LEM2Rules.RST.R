dfeda94e7faf09555766cce99522f77f764f85e0
Hong Ong <hong_ong@knorex.com>
2017-06-15 15:58:34 +0800

    Demo rules induction

---
+++ roughset_demo/RI.LEM2Rules.RST.R
@@ -0,0 +1,37 @@
+###########################################################
+## Example
+##############################################################
+library(RoughSets)
+
+# get data file name argument
+args <- commandArgs(TRUE)
+data_name <- as.character(args[1])
+
+# loading data to data frame
+wine.data <- read.csv(data_name)
+wine.data
+
+## Split the data into a training set and a test set,
+## 60\% for training and 40\% for testing:
+idx <- round(0.6 * nrow(wine.data))
+wine.tra <-SF.asDecisionTable(wine.data[1:idx,],
+                              decision.attr = 5)
+wine.tst <- SF.asDecisionTable(wine.data[(idx+1):nrow(wine.data), -ncol(wine.data)])
+
+true.classes <- wine.data[(idx+1):nrow(wine.data), ncol(wine.data)]
+
+## discretization:
+cut.values <- D.discretize.equal.intervals.RST(wine.tra, nOfIntervals = 4)
+data.tra <- SF.applyDecTable(wine.tra, cut.values)
+data.tst <- SF.applyDecTable(wine.tst, cut.values)
+
+## rule induction from the training set:
+rules <- RI.LEM2Rules.RST(data.tra)
+rules
+
+## predicitons for the test set:
+pred.vals <- predict(rules, data.tst)
+
+## checking the accuracy of predictions:
+mean(pred.vals == true.classes)
+
